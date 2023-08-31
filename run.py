57b2370bb17fd52da228013d1975f8e21db09bf1
Mannio98 <>
2023-08-31 06:13:26 +0000

    Add list of words to be guessed

---
+++ run.py
@@ -0,0 +1,6 @@
+import random
+
+words = [
+         "Mango", "Frolic", "Turtle", "Whisker", "Puzzle", "Guitar", "Rabbit", "Velvet", "Orange", 
+        "Sunset", "Banana", "Dazzle", "Candle", "Rocket", "Laptop", "Orchid", "Whales", "Jungle", "Plaque", "Fizzle"
+          ]
